2f98957779da02395c2a591c6d1f5926bf1c272d
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2026-07-21 22:30:32 +0530

    init pod-workflow

---
+++ pod-workflow/src/main/java/com/bxtralabs/podworkflow/PodWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.bxtralabs.podworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodWorkflowApplication.class, args);
+	}
+
+}
