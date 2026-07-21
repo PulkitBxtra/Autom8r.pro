180cbf6157f6a39a7d35779a2709826f6cb6f0d1
Pulkit Batra <95132680+PulkitBxtra@users.noreply.github.com>
2026-07-21 22:30:32 +0530

    init pod-workflow

---
+++ pod-workflow/src/test/java/com/bxtralabs/podworkflow/PodWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bxtralabs.podworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
